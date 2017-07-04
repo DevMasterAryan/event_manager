7fbc5fed4a5a55459c3e05c36c6d9a13160ccd48
DevMasterAryan <you@example.com>
2017-07-04 11:29:02 +0530

    comment added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
  has_many :attended_events, through: :event_attendings, source: :attended_event
 
  has_many :created_events, foreign_key: :manager_id, class_name: "Event", dependent: :destroy
-
+#validattion goes here
  validates :name, presence: true
  
 end
